0f9dfe7b34532e82b26b7c2862608e01f81924f2
Mrpi314tech <elliotedebruin@gmail.com>
2024-11-10 21:08:14 -0500

    Add headers

--- main.py
+++ main.py
@@ -10,7 +10,15 @@ def scrape(url):
     surl=url
     url=url.replace('+','plus')
     url="https://www.google.com/search?q="+url.replace(' ','+')
-    response = requests.get(url)
+    
+    headers = {
+        "Accept-Language": "en-US,en;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+    }
+    response = requests.get(url, headers=headers)
     
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
